a6e8034b4387bd155afeecdd88d933cafd856c30
Bart Frenk <bart.frenk@gmail.com>
2025-10-27 22:23:34 +0100

    Add Rust solution for part 2 of day 1

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,5 +2,6 @@ mod solutions;
 use crate::solutions::day1;
 
 fn main() {
-    println!("Day 1: {}", day1::result());
+    let (part1, part2) = day1::result();
+    println!("Day 1: {} {}", part1, part2);
 }
--- rust/src/solutions/day1.rs
+++ rust/src/solutions/day1.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
@@ -20,15 +21,24 @@ fn read_input(path: impl AsRef<Path>) -> io::Result<(Vec<i64>, Vec<i64>)> {
     Ok((xs, ys))
 }
 
-fn compute_result(xs: &mut Vec<i64>, ys: &mut Vec<i64>) -> i64 {
+fn compute_part1(xs: &mut Vec<i64>, ys: &mut Vec<i64>) -> i64 {
     xs.sort();
     ys.sort();
     xs.iter().zip(ys).map(|(x, y)| (*x - *y).abs()).sum()
 }
 
-pub fn result() -> i64 {
+fn compute_part2(xs: &Vec<i64>, ys: &Vec<i64>) -> i64 {
+    let mut counts = HashMap::new();
+    for y in ys {
+        let count = counts.entry(*y).or_insert(0);
+        *count += 1;
+    }
+    xs.iter().map(|x| counts.get(x).unwrap_or(&0) * x).sum()
+}
+
+pub fn result() -> (i64, i64) {
     let (mut xs, mut ys) = read_input("inputs/day1.txt").unwrap();
-    compute_result(&mut xs, &mut ys)
+    (compute_part1(&mut xs, &mut ys), compute_part2(&xs, &ys))
 }
 
 #[cfg(test)]
@@ -36,9 +46,16 @@ mod tests {
     use crate::solutions::day1::*;
 
     #[test]
-    fn test_example() {
+    fn test_example_part1() {
         let mut xs = vec![3, 4, 2, 1, 3, 3];
         let mut ys = vec![4, 3, 5, 3, 9, 3];
-        assert!(compute_result(&mut xs, &mut ys) == 11);
+        assert!(compute_part1(&mut xs, &mut ys) == 11);
+    }
+
+    #[test]
+    fn test_example_part2() {
+        let xs = vec![3, 4, 2, 1, 3, 3];
+        let ys = vec![4, 3, 5, 3, 9, 3];
+        assert!(compute_part2(&xs, &ys) == 31);
     }
 }
