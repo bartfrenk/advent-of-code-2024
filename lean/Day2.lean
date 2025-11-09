8e474ee1e1ee54b004b5b8aa0312dbfe1e91b12c
Bart Frenk <bart.frenk@gmail.com>
2025-11-09 23:57:28 +0100

    Finish Day2 solution in Lean

--- lean/Day2.lean
+++ lean/Day2.lean
@@ -1,4 +1,4 @@
-open IO.FS (Stream Handle)
+open IO.FS (Stream Handle Mode)
 
 abbrev Report := List Nat
 
@@ -8,7 +8,7 @@ def openFile (path : System.FilePath) : ResultT IO Stream := do
   if !(← path.pathExists) then
     throw s!"File not found {path}"
   else do
-    let handle ← Handle.mk path IO.FS.Mode.read
+    let handle ← Handle.mk path Mode.read
     pure (Stream.ofHandle handle)
 
 partial def processLines (parse : String -> Option α) (s : Stream): ResultT IO (List α) := do
@@ -24,6 +24,25 @@ def parseLine (s : String): Option Report :=
   let isNotEmpty := not ∘ String.isEmpty
   List.mapM String.toNat? (List.filter isNotEmpty s.splitOn)
 
+def isSafe (report : Report) : Bool :=
+  let rec boundedIncreasing: Report → Bool
+    | [] => true
+    | [_] => true
+    | x1::x2::xs => x1 < x2 && x2 <= x1 + 3 && boundedIncreasing (x2::xs)
+  boundedIncreasing report || boundedIncreasing (List.reverse report)
+
+def part1 (reports : List Report): Nat :=
+  List.length (List.filter isSafe reports)
+
+def subReports (report : Report): List Report :=
+  skip report <$> List.range (report.length)
+  where
+    skip (report : Report) (i : Nat): List Nat :=
+      List.append (report.take i) (report.drop (i + 1))
+
+def part2 (reports : List Report): Nat :=
+  List.length (List.filter (flip List.any isSafe ∘ subReports) reports)
+
 def main (args: List String): IO UInt32 :=
   match args with
     | [path] => do
@@ -34,7 +53,8 @@ def main (args: List String): IO UInt32 :=
         stdout.putStrLn s
         pure 2
       | Except.ok reports => do
-        reports.forM (stdout.putStrLn ∘ toString)
+        stdout.putStrLn s!"Part1: {part1 reports}"
+        stdout.putStrLn s!"Part2: {part2 reports}"
         pure 0
     | _ => pure 1
 
