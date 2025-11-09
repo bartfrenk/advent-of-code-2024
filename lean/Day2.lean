0872e76b0d47f43b5271a3fe75b222d4d84f3db0
Bart Frenk <bart.frenk@gmail.com>
2025-11-09 23:26:49 +0100

    Input reading routine in Lean for day2 solution

--- lean/Day2.lean
+++ lean/Day2.lean
@@ -1,30 +1,40 @@
+open IO.FS (Stream Handle)
 
-def main : IO Unit := do
-  (← IO.getStdout).putStrLn "Hello Day2"
+abbrev Report := List Nat
 
-def openFile (path : System.FilePath) : IO (Option IO.FS.Stream) := do
+abbrev ResultT := ExceptT String
+
+def openFile (path : System.FilePath) : ResultT IO Stream := do
   if !(← path.pathExists) then
-    (← IO.getStderr).putStrLn s!"File not found {path}"
-    pure none
-  else
-    let handle ← IO.FS.Handle.mk path IO.FS.Mode.read
-    pure (some (IO.FS.Stream.ofHandle handle))
+    throw s!"File not found {path}"
+  else do
+    let handle ← Handle.mk path IO.FS.Mode.read
+    pure (Stream.ofHandle handle)
 
-partial def processLines (stream : IO.FS.Stream) (f : String -> α): IO (List α)
-  := do
-    let line ← stream.getLine
-    if line.length > 0 then
-      let tail ← processLines stream f
-      pure (f (line.dropRight 1) :: tail) -- Drop the final linebreak
-    else
-      pure []
+partial def processLines (parse : String -> Option α) (s : Stream): ResultT IO (List α) := do
+  let line ← flip String.dropRight 1 <$> s.getLine
+  if line.length == 0 then pure []
+  else match parse line with
+    | none => throw s!"Failed to parse line {line}"
+    | some x => do
+      let xs ← processLines parse s
+      pure (x::xs)
 
-def parseLine (s : String): Option (List Nat) :=
-  let isNonZero (str: String) := str.length > 0
-  List.mapM String.toNat? (List.filter isNonZero s.splitOn)
+def parseLine (s : String): Option Report :=
+  let isNotEmpty := not ∘ String.isEmpty
+  List.mapM String.toNat? (List.filter isNotEmpty s.splitOn)
 
-def readFile (path : System.FilePath): IO (Option (List Nat)) := do
-  let stream' ← openFile path
-  match stream' with
-  | none => pure none
-  | some stream => processLines stream parseLine
+def main (args: List String): IO UInt32 :=
+  match args with
+    | [path] => do
+      let result ← ExceptT.run (openFile path >>= processLines parseLine)
+      let stdout ← IO.getStdout
+      match result with
+      | Except.error s => do
+        stdout.putStrLn s
+        pure 2
+      | Except.ok reports => do
+        reports.forM (stdout.putStrLn ∘ toString)
+        pure 0
+    | _ => pure 1
+
