73213c162951503465e42b9f7fdcd53f02d63e5f
Bart Frenk <bart.frenk@gmail.com>
2025-11-10 23:24:22 +0100

    Add part1 of day 4 solution in Rust

---
+++ rust/src/day4.rs
@@ -0,0 +1,94 @@
+use std::env::args;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, Error};
+
+use itertools::iproduct;
+use std::process::exit;
+
+type Board = Vec<Vec<u8>>;
+
+fn read<S: io::Read>(stream: &mut S) -> Result<Board, Error> {
+    let mut board: Board = Vec::new();
+    for result in io::BufReader::new(stream).lines() {
+        match result {
+            Err(error) => return Err(error),
+            Ok(line) => board.push(Vec::from(line.as_bytes())),
+        }
+    }
+    return Ok(board);
+}
+
+fn has_word(board: &Board, pos: (usize, usize), dir: (i8, i8), word: &[u8]) -> bool {
+    let mut x: i64 = pos.0 as i64;
+    let mut y: i64 = pos.1 as i64;
+    for k in 1..word.len() {
+        x += dir.0 as i64;
+        y += dir.1 as i64;
+        if x < 0 || y < 0 || x >= (board.len() as i64) || y >= (board[0].len() as i64) {
+            return false;
+        } else if word[k] != board[x as usize][y as usize] {
+            return false;
+        }
+    }
+    return true;
+}
+
+fn directions() -> impl Iterator<Item = (i8, i8)> {
+    iproduct!([-1, 0, 1], [-1, 0, 1]).filter(|(i, j)| *i != 0 || *j != 0)
+}
+
+fn part1(board: &Board, word: &[u8]) -> usize {
+    let mut total = 0;
+    for r in 0..board.len() {
+        for c in 0..board[r].len() {
+            if board[r][c] == word[0] {
+                for (i, j) in directions() {
+                    if has_word(&board, (r, c), (i, j), word) {
+                        total += 1;
+                    }
+                }
+            }
+        }
+    }
+    return total;
+}
+
+fn main() {
+    match args().skip(1).next() {
+        None => exit(1),
+        Some(path) => {
+            let mut file = File::open(&path).unwrap();
+            if let Ok(board) = read(&mut file) {
+                println!("Part1: {}", part1(&board, "XMAS".as_bytes()));
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_small_example_part1() {
+        let board = vec![
+            Vec::from("MMMSXXMASM".as_bytes()),
+            Vec::from("MSAMXMSMSA".as_bytes()),
+            Vec::from("AMXSXMAAMM".as_bytes()),
+            Vec::from("MSAMASMSMX".as_bytes()),
+            Vec::from("XMASAMXAMM".as_bytes()),
+            Vec::from("XXAMMXXAMA".as_bytes()),
+            Vec::from("SMSMSASXSS".as_bytes()),
+            Vec::from("SAXAMASAAA".as_bytes()),
+            Vec::from("MAMMMXMMMM".as_bytes()),
+            Vec::from("MXMXAXMASX".as_bytes()),
+        ];
+        // for line in board {
+        //     println!("{:?}", line)
+        // }
+        let res = part1(&board, b"XMAS");
+
+        println!("{}", res);
+    }
+}
